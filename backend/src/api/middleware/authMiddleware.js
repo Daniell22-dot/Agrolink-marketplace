811885fdeb680b004762627c56ee6e9c38cfec1b
Daniell22-dot <manyasadaniel630@gmail.com>
2026-09-23 23:15:04 +0300

    Modify order detail page

--- backend/src/api/middleware/authMiddleware.js
+++ backend/src/api/middleware/authMiddleware.js
@@ -41,5 +41,16 @@ exports.authorize = (...roles) => {
   };
 };
 
+exports.authorize = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return res.status(403).json({ 
+        message: `Role ${req.user.role} is not authorized` 
+      });
+    }
+    next();
+  };
+};
+
 // Alias for consistency
 exports.authenticate = exports.protect;
